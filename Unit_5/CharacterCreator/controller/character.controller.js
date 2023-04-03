1cc1334db8656f50639a251ccdd7c78074d84eb3
Rich <c0deyes@protonmail.com>
2023-04-03 17:21:03 -0400

    finish character

--- Unit_5/CharacterCreator/controller/character.controller.js
+++ Unit_5/CharacterCreator/controller/character.controller.js
@@ -1,76 +1,200 @@
 const router = require("express").Router();
 const db = require("../helpers/db.json");
 const fs = require("fs");
+const fsPath = "./helpers/db.json";
 
 // GET ALL ROUTE - Read
 router.get("/", (req, res) => {
-    try {
-      res.status(200).json({
-        results: db,
-      });
-    } catch (err) {
-      res.status(500).json({
-        error: err.message,
-      });
-    }
-  });
-
-
+  try {
+    res.status(200).json({
+      results: db,
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    });
+  }
+});
 
 // GET One By ID - Read
-router.get("/:id", (req, res)=>{
-    try {
-        const id = req.params.id;
+router.get("/:id", (req, res) => {
+  try {
+    const id = req.params.id;
 
-        let character = db.filter(obj => obj.id == id);
+    let character = db.filter((obj) => obj.id == id);
 
-res.status(200).json({
+    res.status(200).json({
       status: `Found item at id: ${id}`,
       character,
     });
-
-
-    }catch(err){
-        res.status(500).json({
-            error: err.message,
-          });
-
-    }
-
-})
-
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    });
+  }
+});
 
 // GET ALL By Class - Read
-router.get("/class/:class", (req, res) =>{
-    try {
-        const characterClass = req.params.class;
+router.get("/class/:class", (req, res) => {
+  try {
+    const characterClass = req.params.class;
 
-        let charResults = db.filter(obj => obj.class.toLowerCase() === characterClass.toLowerCase());
+    let charResults = db.filter(
+      (obj) => obj.classType.toLowerCase() === characterClass.toLowerCase()
+    );
 
-        if(charResults.length === 0){
-            res.status(401).json({
-                message: `There are no ${characterClass}s.`
-
-            })
-        }
-        res.status(200).json({
-            character: charResults,
-        });
-
-    } catch (err){
-        res.status(500).json({
-            error: err.message
-        })
+    if (charResults.length === 0) {
+      res.status(401).json({
+        message: `There are no ${characterClass}s.`,
+      });
     }
+    res.status(200).json({
+      character: charResults,
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    });
+  }
+});
 
-})
+// POST One - Create, http://localhost:4001/characters/create
+router.post("/create", (req, res) => {
+  try {
+    // Object Destructuring to help individually grab the keys & values (properties) of our character object coming from req.body
+    let { name, classType, age, race, npc } = req.body;
 
+    // Use math to create an id for the new character
+    let newId = db.length + 1;
 
-// POST One - Create
+    // Declare and assign newChar object
+    const newCharacter = {
+      id: newId,
+      name,
+      classType,
+      age,
+      race,
+      npc,
+      level: 1,
+    };
+
+    fs.readFile(fsPath, (err, data) => {
+      if (err) throw err;
+
+      const database = JSON.parse(data);
+
+      // create a way to make sure nothing has the same ID
+      console.log(
+        "ID values: ",
+        database.filter((d) => {
+          if (d) {
+            return d.id;
+          }
+        })
+      );
+
+      let currentIDs = [];
+
+      database.forEach((obj) => {
+        currentIDs.push(obj.id);
+      });
+      if (currentIDs.includes(newId)) {
+        let maxValue = Math.max(...currentIDs);
+        newId = maxValue + 1;
+        newCharacter.id = newId;
+      }
+      database.push(newCharacter);
+
+      fs.writeFile(fsPath, JSON.stringify(database), (err) => {
+        console.log(err);
+      });
+
+      res.status(200).json({
+        status: `Created new character ${newCharacter.name}!`,
+        newCharacter,
+      });
+    });
+
+    console.log(req.body);
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    });
+  }
+});
 
 // Put One by ID - Update
+router.put("/:id", (req, res) => {
+  try {
+    const id = Number(req.params.id);
 
+    const updatedInfo = req.body;
+    fs.readFile(fsPath, (err, data) => {
+      if (err) throw err;
+
+      const database = JSON.parse(data);
+
+      let character;
+
+      database.forEach((obj, i) => {
+        if (obj.id === id) {
+          let buildObj = {};
+
+          for (key in obj) {
+            if (updatedInfo[key]) {
+              console.log("Checked");
+              buildObj[key] = updatedInfo[key];
+            } else {
+              buildObj[key] = obj[key];
+            }
+          }
+
+          database[i] = buildObj;
+          character = buildObj;
+        }
+      });
+      // Error message for it that id isn't in the data base (db)
+      if (Object.keys(character).length <= 0)
+        res.status(404).json({ message: "No character in roster" });
+
+      fs.writeFile(fsPath, JSON.stringify(database), (err) => console.log(err));
+
+      res.status(200).json({
+        status: `Modified character at ID: ${id}.`,
+        character: character,
+      });
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    });
+  }
+});
 // DELETE One by ID - Delete
 
+router.delete("/:id", (req, res) => {
+  try {
+    const id = Number(req.params.id);
+
+    fs.readFile(fsPath, (err, data) => {
+      if (err) throw err;
+
+      const db = JSON.parse(data);
+      const filteredDb = db.filter((i) => i.id !== id);
+
+      fs.writeFile(fsPath, JSON.stringify(filteredDb), (err) =>
+        console.log(err)
+      );
+
+      res.status(200).json({
+        status: `ID: ${id} was successfully deleted.`,
+      });
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    });
+  }
+});
 
 module.exports = router;
