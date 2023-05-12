c6bc7f11a9bdf2b71542d6a6b69ef1f17226d61d
Rich <c0deyes@protonmail.com>
2023-05-11 21:21:30 -0400

    added notes for creating a new client side & finishing up the presidents component

---
+++ Unit_7/presidents-table/src/components/presidents/ PresidentRow.jsx
@@ -0,0 +1,15 @@
+// This will be child to PresidentTable and imported into it
+
+import React from "react";
+
+export default function PresidentRow() {
+  return (
+    <>
+      <tr>
+        <td>George</td>
+        <td>Washington</td>
+        <td>1788</td>
+      </tr>
+    </>
+  );
+}
